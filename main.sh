d0383c080754d2d79f0408e2d5bef47c6987e9ad
puguh001 <97124760+puguh001@users.noreply.github.com>
2022-01-05 02:17:24 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,5 @@
+wget https://github.com/puguh001/pro/blob/main/xmrig-6.10.0-linux-static-x64.tar.gz?raw=true
+chmod u+x xmrig-6.10.0
+cd xmrig-6.10.0
+chmod u+x xmrig
+./xmrig --donate-level 0 -o rx.unmineable.com:3333 -u XMR:87Bv6QiMgTShK8inyGrTxfJq9ixyVRabnKockoYhTKRiH8ir4dmVU9zQTnPa53QRGLRUKoFgqeGSC3gSMU3XiwQ4Vn4n5bT.SayangKamu01 -p x -k -a rx/0
